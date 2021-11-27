3f4ef2264f360e28aeb5dbb8cc14303437eb1e6e
ningbao <957926787@qq.com>
2021-11-27 17:59:46 +0800

    新增直方图均衡和高斯滤波器

--- final/ImageHandler.py
+++ final/ImageHandler.py
@@ -56,6 +56,11 @@ class ImageHandler:
         # 输入图像 4096级灰度图像
         self.image = image
 
+    """
+    坐标原点为图像左上角
+    坐标轴x（height）向下，坐标轴y（width）向右
+    """
+
     # 灰度化处理
     def toGreyImage(self):
         pass
@@ -100,7 +105,7 @@ class ImageHandler:
                 # 坐标变换
                 pos_vec = T.translate(pos_vec, [center_h, center_w])
                 # zoom
-                pos_vec = T.scale(pos_vec, 1/scale_h, 1/scale_w)
+                pos_vec = T.scale(pos_vec, 1 / scale_h, 1 / scale_w)
                 # 坐标变换
                 pos_vec = T.translate(pos_vec, [-center_h, -center_w])
 
@@ -118,20 +123,172 @@ class ImageHandler:
     def detailAugment(self):
         pass
 
+    # 图像锐化
+    def sharpen(self):
+        pass
+
+    # 直方图均衡
+    # input 4096级灰度
+    # output 4096级灰度
+    def histEqual(self):
+        gray_max = 4096
+
+        height = self.image.shape[0]
+        width = self.image.shape[1]
+
+        # 灰度分布直方图
+        hist = [0 for x in range(gray_max)]
+        # 计算index之前的所有hist的和
+        trans_func = [0 for x in range(gray_max)]
+
+        # 统计灰度等级
+        for x in range(height):
+            for y in range(width):
+                hist[self.image[x][y]] += 1
+
+        total = 0
+        for i in range(gray_max):
+            total += hist[i]
+            trans_func[i] = round(total * gray_max / height / width)
+
+        result = np.empty([height, width], dtype=int)
+        for x in range(height):
+            for y in range(width):
+                result[x][y] = trans_func[self.image[x][y]]
+
+        return result
+
+    # 平均空间滤波器
+    def averageFilter(self, kernel_size=3, method="replicate"):
+        height = self.image.shape[0]
+        width = self.image.shape[1]
+
+        result = np.empty([height, width], dtype=int)
+        k_filter = np.ones((kernel_size, kernel_size), dtype="uint8")
+
+        for i in range(height):
+            for j in range(width):
+
+                # 在核内遍历
+                total = 0
+                for k in range(pow(kernel_size, 2)):
+                    # 在核内的坐标，以左上角为中心
+                    k_row = k // kernel_size
+                    k_col = k % kernel_size
+
+                    # 在核内的坐标，以核中心为原地
+                    row_bias = int(k_row - (kernel_size - 1) / 2)
+                    col_bias = int(k_col - (kernel_size - 1) / 2)
+
+                    f_value = k_filter[k_row][k_col]
+                    # 特殊情况处理
+                    if i + row_bias <= 0:
+                        if j + col_bias <= 0:
+                            total += self.image[0][0] * f_value
+                        elif j + col_bias >= width - 1:
+                            total += self.image[0][-1] * f_value
+                        else:
+                            total += self.image[0][j + col_bias] * f_value
+                    elif i + row_bias >= height - 1:
+                        if j + col_bias <= 0:
+                            total += self.image[-1][0] * f_value
+                        elif j + col_bias >= width - 1:
+                            total += self.image[-1][-1] * f_value
+                        else:
+                            total += self.image[-1][j + col_bias] * f_value
+                    else:
+                        if j + col_bias <= 0:
+                            total += self.image[i + row_bias][0] * f_value
+                        elif j + col_bias >= width - 1:
+                            total += self.image[i + row_bias][-1] * f_value
+                        else:
+                            total += self.image[i + row_bias][j + col_bias] * f_value
+
+                total = total / pow(kernel_size, 2)
+                result[i][j] = total
+
+        return result
+
+    # 高斯滤波器
+    def gaussianFilter(self, kernel_size=3, sigma=1, method="replicate"):
+        height = self.image.shape[0]
+        width = self.image.shape[1]
+
+        result = np.empty([height, width], dtype=int)
+
+        k_filter = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
+        # k_filter = np.zeros((kernel_size, kernel_size))
+        # mid = (kernel_size - 1) / 2
+        # sigma2 = pow(sigma, 2)
+        # for i in range(kernel_size):
+        #     for j in range(kernel_size):
+        #         tmp = -(pow(i - mid, 2) + pow(j - mid, 2)) / 2 / sigma2
+        #         k_filter[i, j] = math.exp(tmp) / 2 / 3.14 / sigma2
+        #
+        # filter_sum = k_filter.sum()
+
+        for i in range(height):
+            for j in range(width):
+
+                # 在核内遍历
+                total = 0
+                for k in range(pow(kernel_size, 2)):
+                    # 在核内的坐标，以左上角为中心
+                    k_row = k // kernel_size
+                    k_col = k % kernel_size
+
+                    # 在核内的坐标，以核中心为原地
+                    row_bias = int(k_row - (kernel_size - 1) / 2)
+                    col_bias = int(k_col - (kernel_size - 1) / 2)
+
+                    f_value = k_filter[k_row][k_col]
+                    # 特殊情况处理
+                    if i + row_bias <= 0:
+                        if j + col_bias <= 0:
+                            total += self.image[0][0] * f_value
+                        elif j + col_bias >= width - 1:
+                            total += self.image[0][-1] * f_value
+                        else:
+                            total += self.image[0][j + col_bias] * f_value
+                    elif i + row_bias >= height - 1:
+                        if j + col_bias <= 0:
+                            total += self.image[-1][0] * f_value
+                        elif j + col_bias >= width - 1:
+                            total += self.image[-1][-1] * f_value
+                        else:
+                            total += self.image[-1][j + col_bias] * f_value
+                    else:
+                        if j + col_bias <= 0:
+                            total += self.image[i + row_bias][0] * f_value
+                        elif j + col_bias >= width - 1:
+                            total += self.image[i + row_bias][-1] * f_value
+                        else:
+                            total += self.image[i + row_bias][j + col_bias] * f_value
+
+                # total = total / pow(kernel_size, 2)
+                total = total / 16
+                # total = total / filter_sum
+                result[i][j] = total
+
+        return result
+
 
 if __name__ == '__main__':
     reader = ImageReader("images_raw/lumbar.raw")
     # reader = ImageReader("test.raw")
     # print(b'\x01' + b'\x02')
     image_array = reader.getImageArray()
-    image_array_raw = reader.getImageArrayRaw()
     print(reader.image_height, reader.image_width)
 
-    # img_origin = Image.fromarray(image_array.astype('uint8'), mode="L")
-    # img_origin.save("origin.png")
+    img_origin = Image.fromarray(image_array.astype('uint8'), mode="L")
+    img_origin.show()
 
+    image_array_raw = reader.getImageArrayRaw()
     handler = ImageHandler(image_array_raw)
-    image_temp = handler.partZoom(800, 800, 3, 3, 1000, 1000)
+    # image_temp = handler.partZoom(800, 800, 3, 3, 1000, 1000)     # 局部放大测试
+    # image_temp = T.to8BitImage(handler.histEqual())     # 直方图均衡化测试
+    # image_temp = T.to8BitImage(handler.averageFilter())     # 平均滤波测试
+    image_temp = T.to8BitImage(handler.gaussianFilter())     # 高斯滤波测试
     img_result = Image.fromarray(image_temp.astype('uint8'), mode="L")
     img_result.show()
     # img_result.save("result.png")
@@ -145,4 +302,3 @@ if __name__ == '__main__':
     # img_new.save("test.png")
 
     # 图像展示
-
